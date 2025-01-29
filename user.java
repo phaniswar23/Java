6cf9146590a89491a0d5baec49252d65e67aac93
Phaniswar <phaniswarjanyavula@gmail.com>
2025-01-29 13:23:53 +0530

    Java Done

---
+++ user.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class user {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number 1 :");
+        int a = sc.nextInt();
+        System.out.println("Enter your number 2:");
+        int b = sc.nextInt();
+        sc.close();
+        
+    }
+}
